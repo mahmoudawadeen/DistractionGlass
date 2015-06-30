325c62d06c00efbbc33f9099441ca3624714ad62
mahmoudawadeen <mahmoud.awadeen@gmail.com>
2015-06-30 13:34:16 +0200

    white icons

--- app/src/main/java/com/example/mahmoudawadeen/distractionglass/MainActivity.java
+++ app/src/main/java/com/example/mahmoudawadeen/distractionglass/MainActivity.java
@@ -166,7 +166,7 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onPostExecute(Object result) {
-            imageSwitcher_caps.setImageResource(result.equals("on") ? R.drawable.on_square : R.drawable.off_square);
+            imageSwitcher_caps.setImageResource(result.equals("on") ? R.drawable.on_square_white : R.drawable.off_square_white);
         }
     }
 
